a694c5b5da2f284a9e95647e1c5e2efde7ee3460
Mark Dinglasa <ryanmark.dinglasa@gmail.com>
2024-06-11 04:52:03 +0800

    remove uneccessary components

--- src/apps/registry.tsx
+++ src/apps/registry.tsx
@@ -1,45 +1,20 @@
 import {useSelector} from 'react-redux';
-
-import {AccountManager, AccountManagerRegistration} from 'apps/AccountManager/registration';
-import {Chat, ChatElectronStore, ChatRegistration} from 'apps/Chat/registration';
-import {NetworkManager, NetworkManagerRegistration} from 'apps/NetworkManager/registration';
-import {Shop, ShopElectronStore, ShopRegistration} from 'apps/Shop/registration';
-import {SpeedTest, SpeedTestElectronStore, SpeedTestRegistration} from 'apps/SpeedTest/registration';
-import {Trade, TradeElectronStore, TradeRegistration} from 'apps/Trade/registration';
 import {University, UniversityElectronStore, UniversityRegistration} from 'apps/University/registration';
 import {getManager} from 'system/selectors/state';
 import {AppDataHandlers, AppRegistration, SFC} from 'system/types';
 
 export interface AppsElectronStore
-  extends ChatElectronStore,
-    ShopElectronStore,
-    SpeedTestElectronStore,
-    TradeElectronStore,
-    UniversityElectronStore {}
+  extends UniversityElectronStore {}
 
 export const appReducers = {
-  chat: ChatRegistration.reducer!,
-  shop: ShopRegistration.reducer!,
-  speedTest: SpeedTestRegistration.reducer!,
-  trade: TradeRegistration.reducer!,
   university: UniversityRegistration.reducer!,
 };
 
-export const appRegistrations: AppRegistration[] = [
-  UniversityRegistration,
-  ChatRegistration,
-  TradeRegistration,
-  SpeedTestRegistration,
-  ShopRegistration,
-  AccountManagerRegistration,
-  NetworkManagerRegistration,
+export const appRegistrations: AppRegistration[] = [ 
+  UniversityRegistration 
 ];
 
 export const appRouters: AppDataHandlers = {
-  chat: ChatRegistration.router!,
-  shop: ShopRegistration.router!,
-  speedTest: SpeedTestRegistration.router!,
-  trade: TradeRegistration.router!,
   university: UniversityRegistration.router!,
 };
 
@@ -48,13 +23,7 @@ export const Apps: SFC = () => {
 
   return (
     <>
-      <AccountManager display={activeApp === AccountManagerRegistration.appId} />
-      <Chat display={activeApp === ChatRegistration.appId} />
-      <NetworkManager display={activeApp === NetworkManagerRegistration.appId} />
-      <Shop display={activeApp === ShopRegistration.appId} />
-      <SpeedTest display={activeApp === SpeedTestRegistration.appId} />
-      <Trade display={activeApp === TradeRegistration.appId} />
-      <University display={activeApp === UniversityRegistration.appId} />
+      <University display={activeApp} />
     </>
   );
 };
