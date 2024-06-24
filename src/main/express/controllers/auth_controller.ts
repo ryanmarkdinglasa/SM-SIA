d55a17e1e33b24002b4cb6502b3db3cf3d7846c3
Mark Dinglasa <ryanmark.dinglasa@gmail.com>
2024-06-25 06:32:29 +0800

    fix login controller

--- src/main/express/controllers/auth_controller.ts
+++ src/main/express/controllers/auth_controller.ts
@@ -8,7 +8,7 @@
 
 import { LoginSchema } from '../schemas';
 import { ERROR, SUCCESS, CustomRequest } from '../shared'
-import { getUserByUsername, comparePassword, generateToken, generateRefreshToken } from '../functions';
+import { getUserByUsername, generateToken, generateRefreshToken } from '../functions';
 import { Response } from 'express';
 
 export const login = async (req:CustomRequest, res:Response) => {
@@ -19,9 +19,8 @@ export const login = async (req:CustomRequest, res:Response) => {
 
     const user = (await getUserByUsername(Username))[0];
     if (!user) return res.status(400).json({ Login: false, message: ERROR.e00x05 });
-    if (user.isDeactivated === 1)  return res.status(401).json({ Login: false, message: ERROR.e00x20 });
-    
-    const isPasswordValid = await comparePassword(Password, user.Password);
+
+    const isPasswordValid = (Password === user.Password);
     if (!isPasswordValid) return res.status(400).json({ Login: false, message: ERROR.e00x19 });
 
     const accessToken = await generateToken(user.Id);
